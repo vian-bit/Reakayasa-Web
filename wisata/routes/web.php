d1c31553b600afb29608bc75702f6792af2ea003
vian-bit <anakuocta@gmail.com>
2025-10-31 23:47:19 +0700

    Praktikum4

---
+++ wisata/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('wisata',  ['uses' => 'WisataController@showAll']);
+    $router->get('wisata/{id}', ['uses' => 'WisataController@showOne']);
+    $router->post('wisata', ['uses' => 'WisataController@create']);
+    $router->put('wisata/{id}', ['uses' => 'WisataController@update']);
+    $router->delete('wisata/{id}', ['uses' => 'WisataController@delete']);
+});
